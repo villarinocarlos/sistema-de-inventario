5432283a115c1b4aeeb274cc2957b4a5521e9c5c
villarinocarlos <villarinocarlos75@gmail.com>
2024-07-15 20:13:43 -0600

    Añadir todos los archivos del proyecto

--- inventariosistema/inventario/models.py
+++ inventariosistema/inventario/models.py
@@ -1,1 +1,14 @@
 from django.db import models
+
+
+class Inventario(models.Model):
+    nombe = models.CharField(max_length=100, null=False, blank=False)
+    costo_por_item = models.DecimalField(max_digits=19, decimal_places=2, null=False, blank=False)
+    cantidad_en_stock = models.IntegerField(null=False, blank=False)
+    cantidad_vendida = models.IntegerField(null=False, blank=False)
+    ventas = models.DecimalField(max_digits=19, decimal_places=2, null=False, blank=False)
+    stock_fecha = models.DateField(auto_now_add=True)
+    ultima_venta_fecha = models.DateField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.name
